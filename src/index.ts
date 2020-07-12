221e7e8f7a1db5feec6520787da7b39bbd80292a
David Gundersen <gundy@users.noreply.github.com>
2020-07-12 23:00:37 +1000

    fix broken build - incorrect capitalisation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 
 export { Instrument } from "./Instrument"
 export { Mixer } from "./Mixer"
-export { MODLoader } from "./formats/mod/ModLoader"
+export { MODLoader } from "./formats/mod/MODLoader"
 export { S3MLoader } from "./formats/s3m/S3MLoader"
 export { Player } from "./Player"
 export { Sample } from "./Sample"
